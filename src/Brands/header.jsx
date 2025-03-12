36477b8e1f57f3092d4545cd74eb54a80326858b
Muhammad Ghazanfar <116085344+GhazanfarGw@users.noreply.github.com>
2025-03-12 18:32:35 +0500

    Update header.jsx

--- src/Brands/header.jsx
+++ src/Brands/header.jsx
@@ -1,4 +1,4 @@
-limport React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 
 
@@ -95,6 +95,14 @@ function Logo({ isScrolled }) {
                             Environmental Care
                           </a>
                           <a
+                            href="/about-us"
+                            className="
+                            
+                            items-center text-base md:text-sm md:hover:border-b border-[#000000] relative h-8 duration-200 lg:px-8 md:px-4 px-4 flex justify-center flex-row text-[#777777] hover:text-black font-semibold tracking-wide"
+                          >
+                            About Us
+                          </a>
+                          <a
                             href="/contact-us"
                             className="
                             items-center text-base md:text-sm hover:bg-[#403f68] rounded-full border-[#000000] border relative h-10 duration-200 lg:px-8 md:px-4 px-4 flex justify-center flex-row text-[#777777] hover:text-black font-semibold tracking-wide"
@@ -136,6 +144,11 @@ function Logo({ isScrolled }) {
                     Environmental Care
                   </Link>
                 </li>
+                <li>
+                  <Link to="/about-us" className="tracking-wider hover:bg-[#f1f1f1] px-8 py-3 flex flex-row items-center duration-200 border-b hover:text-[#A39896] text-[#403f68] border-[#403f68] border-opacity-10 text-sm font-semibold">
+                    About Us
+                  </Link>
+                </li>
                   <p className='text-sm px-8 text-[#403f68] pt-8'>
                     Start your journey today with us, and discover how we are driving positive change through sustainable investments,
                   </p>
@@ -157,7 +170,7 @@ function Logo({ isScrolled }) {
                 <img width={220} className='block md:hidden lg:hidden items-center duration-500' src='./logo.png' alt='favicon.png'/>
               </a>
             </div>
-            <div className="z-20 text-3xl md:hidden pt-3 px-3 text-black" onClick={() => setOpen(!open)}>
+            <div className="z-20 text-3xl md:hidden pt-3 px-3 text-white" onClick={() => setOpen(!open)}>
               <ion-icon className="text-black" name={`${open ? "close" : "menu"}`}></ion-icon>
             </div>
             <div className="hidden lg:block w-full duration-500 scroll-smooth z-40">
@@ -204,6 +217,14 @@ function Logo({ isScrolled }) {
                     Environmental Care
                   </a>
                   <a
+                    href="/about-us"
+                    className="
+                    
+                    items-center text-base md:text-sm md:hover:border-b border-[#000000] relative h-8 duration-200 lg:px-8 md:px-4 px-4 flex justify-center flex-row text-[#777777] hover:text-black font-semibold tracking-wide"
+                  >
+                    About Us
+                  </a>
+                  <a
                     href="/contact-us"
                     className="
                     items-center text-base md:text-sm rounded-full border-[#201F51] border relative h-10 duration-200 lg:px-8 md:px-4 px-4 flex justify-center flex-row text-[#000] hover:text-black font-semibold tracking-wide"
@@ -243,6 +264,11 @@ function Logo({ isScrolled }) {
                 <li>
                   <Link to="/environmental-care" className="tracking-wider hover:bg-[#f1f1f1] px-8 py-3 flex flex-row items-center duration-200 border-b hover:text-[#A39896] text-[#403f68] border-[#403f68] border-opacity-10 text-sm font-semibold">
                     Environmental Care
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about-us" className="tracking-wider hover:bg-[#f1f1f1] px-8 py-3 flex flex-row items-center duration-200 border-b hover:text-[#A39896] text-[#403f68] border-[#403f68] border-opacity-10 text-sm font-semibold">
+                    About Us
                   </Link>
                 </li>
                   <p className='text-sm px-8 text-[#403f68] pt-8'>
