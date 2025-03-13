f8af62eabd7fc34b5a8600ff7188cd1bdd91bf10
Muhammad Ghazanfar <116085344+GhazanfarGw@users.noreply.github.com>
2025-03-13 08:27:22 +0500

    Add files via upload

---
+++ src/Products/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "./header";
+import Footer from "../Home/Footer";
+import Products from "./Products";
+
+
+export default function Index() {
+  return (
+    <>
+    <Header/>
+    <Products/>
+    <Footer/>
+    </>
+  )
+}
