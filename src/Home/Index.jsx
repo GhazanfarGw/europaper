f7fcaf0c74cb4d703b7383060ab1e30910e44a83
Muhammad Ghazanfar <116085344+GhazanfarGw@users.noreply.github.com>
2025-03-13 10:16:32 +0500

    Update Index.jsx

--- src/Home/Index.jsx
+++ src/Home/Index.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import Header from "./header";
 import Footer from "./Footer";
 import Dashboard from "./Dashboard";
-import About from "./About";
 import Subscribe from './Subscribe';
-import Difference from './Difference';
 
 function Index() {
   return (
     <>
     <Header/>
     <Dashboard/>
-    <About/>
-    <Difference/>
     <Subscribe/>
     <Footer/>
     </>
