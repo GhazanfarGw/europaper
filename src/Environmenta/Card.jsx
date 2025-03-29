c91fe54a073d5ee45966ee9a92ff04231d67b860
Muhammad Ghazanfar <116085344+GhazanfarGw@users.noreply.github.com>
2025-03-30 02:02:05 +0500

    Delete src directory

--- src/Environmenta/Card.jsx
+++
@@ -1,8 +0,0 @@
-export function Card({ children, className = "" }) {
-    return <div className={`border p-5 md:h-[12rem] rounded-lg shadow-md ${className}`}>{children}</div>;
-  }
-  
-  export function CardContent({ children }) {
-    return <div className="p-4">{children}</div>;
-  }
-  
