a5956d90886cf3e3049ddf792756d758c3a46910
Muhammad Ghazanfar <116085344+GhazanfarGw@users.noreply.github.com>
2025-05-01 18:20:33 +0500

    Update Subscribe.jsx

--- src/Home/Subscribe.jsx
+++ src/Home/Subscribe.jsx
@@ -14,13 +14,10 @@ export default function Updates() {
             <p data-aos="zoom-in-up" data-aos-delay="300" data-aos-duration="400" className='uppercase font-semibold text-[#1A2B6B] text-sm md:text-base'>
                 STAY UP TO DATE
             </p>
-            <div data-aos="zoom-in-up" data-aos-delay="300" data-aos-duration="400" className='text-[#1A2B6B] md:flex md:grid-cols-2 justify-between mx-auto'>
-                <div data-aos="zoom-in-up" data-aos-delay="300" data-aos-duration="400" className='max-w-md md:py-0 py-10 block md:hidden lg:hidden'>
-                    <img className='mx-auto' src='./headoffice.png' alt='logo'/>
-                </div>
+            <div data-aos="zoom-in-up" data-aos-delay="300" data-aos-duration="400" className='text-[#1A2B6B] mx-auto justify-center'>
                 <div>
                     <h1 className='md:text-4xl text-2xl mx-auto md:pt-20 pt-5'>
-                        Get our latest insights and company <br />announcements. <br/>
+                        Get our latest insights and brcompany <br />announcements. <br/>
                         <span className='text-sm text-[#4b4b4b]'>
                             For event updates, news, and exclusive insights, subscribe to our newsletter.
                         </span>
@@ -28,9 +25,6 @@ export default function Updates() {
                     <div className="pt-5">
                         <Email/>
                     </div>
-                </div>
-                <div data-aos="zoom-in-up" data-aos-delay="300" data-aos-duration="400" className='max-w-md md:py-0 py-10 hidden md:block lg:block'>
-                    <img className='mx-auto' src='./headoffice.png' alt='logo'/>
                 </div>
             </div>
         </div>
